7fc69fd5ca9987fa5b761d53da693b2fee18e9f7
Maxtream_09 <110289636+Maxz-09@users.noreply.github.com>
2024-10-22 02:26:30 +0700

    update chatgpt.php

--- app/public/api/ai/chatgpt.php
+++ app/public/api/ai/chatgpt.php
@@ -21,7 +21,7 @@ function getChatGPTResponse($query, $API = null)
         "nyx_gpt4" => "https://api.nyxs.pw/ai/gpt4?text=" . urlencode($query),
         "nyx_gpt" => "https://api.nyxs.pw/ai/gpt?text=" . urlencode($query),
         "nyx_turbo" => "https://api.nyxs.pw/ai/turbo?text=" . urlencode($query),
-        "nyx_id" => "https://api.nyxs.pw/ai/gpt4o?system=jawab%20sebagai%20cewek%20cantik%20bernama%20Vikaru%20yang%20sangat%20pemalu%20dengan%20kosa%20kata%20yang%20imut&text=" . urlencode($query),
+        "nyx_id" => "https://api.nyxs.pw/ai/gpt4o?system=jawab%20sebagai%20cewek%20cantik%20bernama%20Vikaru%20yang%20sangat%20pemalu%20dengan%20kosa%20kata%20yang%20cuek&text=" . urlencode($query),
     ];
 
     if ($API && isset($urls[$API])) {
