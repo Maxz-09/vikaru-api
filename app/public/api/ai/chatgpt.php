80a0faeb7aac8f861ab4623a6df4960df28a0339
Maxtream_09 <110289636+Maxz-09@users.noreply.github.com>
2024-10-22 03:16:50 +0700

    update chatgpt.php

--- app/public/api/ai/chatgpt.php
+++ app/public/api/ai/chatgpt.php
@@ -19,7 +19,7 @@ function getChatGPTResponse($query, $API = null)
         "nyx_gpt4" => "https://api.nyxs.pw/ai/gpt4?text=" . urlencode($query),
         "nyx_gpt" => "https://api.nyxs.pw/ai/gpt?text=" . urlencode($query),
         "nyx_turbo" => "https://api.nyxs.pw/ai/turbo?text=" . urlencode($query),
-        "vikaru" => "https://api.nyxs.pw/ai/gpt4o?system=jawab%20sebagai%20cewek%20cantik%20bernama%20Vikaru%20yang%20sangat%20manja%20dan%20imut%20dengan%20kosa%20kata%20yang%20genit,%20gaul,%20dan%20sangat%20singkat&text=" . urlencode($query),
+        "vikaru" => "https://api.nyxs.pw/ai/gpt4o?system=jawab%20sebagai%20cewek%20cantik%20bernama%20Vikaru%20yang%20sangat%20manja%20dan%20imut%20dengan%20kosa%20kata%20yang%20genit,%20pemalu,%20dan%20sangat%20singkat&text=" . urlencode($query),
         "akhiro" => "https://akhiro-rest-api.onrender.com/api/gpt4?q=" . urlencode($query),
         "ngodingaja" => "https://api.ngodingaja.my.id/api/gpt?prompt=" . urlencode($query),
     ];
